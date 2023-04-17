c7314151eb41618578ae6f0cd023fe8539aab206
yusrinelson <125978974+yusrinelson@users.noreply.github.com>
2023-04-17 16:29:31 +0200

    debugged code to work

--- js/scripts.js
+++ js/scripts.js
@@ -29,41 +29,45 @@ const STATUS_MAP = {
 
 // Edit below line  
 
-/*BOOK 1 */
-const status = document.querySelector("#book1 .status")
-const reserve = document.querySelector("#book1 .reserve")
-const checkout = document.querySelector("#book1 .checkout")
-const checkin = document.querySelector("#book1 .checkin")
+/*BOOK 1 */  // used document querySelector to get info from html
+const book1 = document.querySelector("#book1")
+const status1 = book1.querySelector(".status")
+const reserve1 = book1.querySelector(".reserve")
+const checkout1 = book1.querySelector(".checkout")
+const checkin1 = book1.querySelector(".checkin")
 
 /*BOOK 2 */
-// status = selector(status)
-// reserve = selector(reserve)
-// checkout = selector(checkout)
-// checkin = selector(checkin)
+const book2 = document.querySelector("#book2")
+const status2 = book2.querySelector(".status")
+const reserve2 = book2.querySelector(".reserve")
+const checkout2 = book2.querySelector(".checkout")
+const checkin2 = book2.querySelector(".checkin")
+
 
 /*BOOK 3 */
-// status = selector(status)
-// reserve = selector(reserve)
-// checkout = selector(checkout)
-// checkin = selector(checkin)
+const book3 = document.querySelector("#book3")
+const status3 = book3.querySelector(".status")
+const reserve3 = book3.querySelector(".reserve")
+const checkout3 = book3.querySelector(".checkout")
+const checkin3 = book3.querySelector(".checkin")
 
-/*BOOK 1 */
-// checkin.0.color = none
-// status.0.style.color = STATUS_MAP.status.color
-reserve.textContent = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+/*BOOK 1 */  //used if statement to check if true or false
+checkin1.style.color = "black"
+status1.style.color = STATUS_MAP.overdue.color
+if (reserve1.disabled = true){STATUS_MAP.overdue.canReserve }
+if (checkout1.disabled = true ){STATUS_MAP.overdue.canCheckout}
+if (checkin1.disabled = false){STATUS_MAP.overdue.canCheckIn}
 
-/*BOOK 2 */
-// checkin.1.color = none
-// status.1.style.color = STATUS_MAP.status.color
-// reserve.1 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-// checkout.1 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-// checkin.1 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+// /*BOOK 2 */
+checkin2.style.color = "black"
+status2.style.color = STATUS_MAP.reserved.color
+if (reserve2.disabled = false){STATUS_MAP.reserved.canReserve }
+if (checkout2.disabled = true ){STATUS_MAP.reserved.canCheckout}
+if (checkin2.disabled = false){STATUS_MAP.reserved.canCheckIn}
 
 /*BOOK 3 */
-// checkin.2.color = none
-// status.2.style.color = STATUS_MAP.status.color
-// reserve.2 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-// checkout.2 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-// checkin.2 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+checkin3.style.color = "black"
+status3.style.color = STATUS_MAP.shelf.color
+if (reserve3.disabled = true){STATUS_MAP.shelf.canReserve }
+if (checkout3.disabled = true ){STATUS_MAP.shelf.canCheckout}
+if (checkin3.disabled = false){STATUS_MAP.shelf.canCheckIn}
